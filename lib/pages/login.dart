7eee3a09b7f6e99ced89ee8949fdda65c56c4c72
Jacob Brasil <jnxtbrasil@gmail.com>
2021-01-16 00:29:20 -0500

    Added basics for Auth as well as Theming

---
+++ lib/pages/login.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  Login({Key key, @required this.loginCallback}) : super(key: key);
+
+  final VoidCallback loginCallback;
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new SafeArea();
+  }
+}
