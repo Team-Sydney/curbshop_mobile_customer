310acb23d6de2578e536258dcd0a1b53eebadcb4
Jacob Brasil <jnxtbrasil@gmail.com>
2021-04-12 20:23:51 -0400

    Added Popup Menu

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,10 +4,13 @@ import 'package:curbshop_mobile_customer/backend/models/Product.dart';
 import 'package:curbshop_mobile_customer/backend/models/Store.dart';
 import 'package:curbshop_mobile_customer/controllers/cartController.dart';
 import 'package:curbshop_mobile_customer/controllers/customPanelController.dart';
+import 'package:curbshop_mobile_customer/themes/themeColors.dart';
 import 'package:curbshop_mobile_customer/widgets/featuredItem.dart';
 import 'package:curbshop_mobile_customer/widgets/header.dart';
 import 'package:curbshop_mobile_customer/widgets/inputField.dart';
+import 'package:curbshop_mobile_customer/widgets/popupMenu.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class HomePage extends StatefulWidget {
   final CustomPanelController customPanelController;
@@ -30,6 +33,8 @@ class HomePage extends StatefulWidget {
 class _HomeState extends State<HomePage> {
   List<Product> featuredItems;
   List<CartProduct> inCart;
+
+  bool showMenu = false;
 
   @override
   void initState() {
@@ -73,57 +78,89 @@ class _HomeState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Container(
-          padding: EdgeInsets.only(left: 20, top: 20, right: 20),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            Header(
-                title: "curbshop",
-                circleImage: NetworkImage(widget.currentCustomer.user.photoUrl),
-                iconButton: (inCart.length > 0
-                    ? Icons.shopping_bag
-                    : Icons.shopping_bag_outlined),
-                amountInCart: inCart.length,
-                iconOnPressed: () => widget.customPanelController.openPanel(),
-                imageOnPressed: () => widget.customPanelController.openPanel()),
-            InputField(
-              label: "Search",
-              icon: Icons.search,
-            )
-          ])),
-      Container(
-          width: MediaQuery.of(context).size.width,
-          height: 400,
-          margin: EdgeInsets.only(top: 20),
-          child: (featuredItems.length > 0
-              ? ListView.builder(
-                  padding: EdgeInsets.only(left: 20, right: 20),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: featuredItems.length,
-                  itemBuilder: (context, index) {
-                    return FeaturedItem(
-                      product: featuredItems[index],
-                      cartOnPress: (product) {
-                        setState(() {
-                          product.inCart = !product.inCart;
+    return Stack(children: [
+      Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+            padding: EdgeInsets.only(left: 20, top: 20, right: 20),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Header(
+                      title: "curbshop",
+                      circleImage:
+                          NetworkImage(widget.currentCustomer.user.photoUrl),
+                      iconButton: (inCart.length > 0
+                          ? Icons.shopping_bag
+                          : Icons.shopping_bag_outlined),
+                      amountInCart: inCart.length,
+                      iconOnPressed: () =>
+                          widget.customPanelController.openPanel(),
+                      imageOnPressed: () => this.showMenu = !this.showMenu),
+                  InputField(
+                    label: "Search",
+                    icon: Icons.search,
+                  )
+                ])),
+        Container(
+            width: MediaQuery.of(context).size.width,
+            height: 400,
+            margin: EdgeInsets.only(top: 20),
+            child: (featuredItems.length > 0
+                ? ListView.builder(
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: featuredItems.length,
+                    itemBuilder: (context, index) {
+                      return FeaturedItem(
+                        product: featuredItems[index],
+                        cartOnPress: (product) {
+                          setState(() {
+                            product.inCart = !product.inCart;
 
-                          if (product.inCart)
-                            widget.cartController
-                                .add(CartProduct(product.pricePer, product));
-                          else
-                            widget.cartController.remove(product: product);
-                        });
-                      },
-                      wishlistOnPress: (product) {
-                        setState(() {
-                          product.isWishlisted = !product.isWishlisted;
-                        });
-                      },
-                    );
-                  },
-                )
-              : Center(child: CircularProgressIndicator.adaptive())))
+                            if (product.inCart)
+                              widget.cartController
+                                  .add(CartProduct(product.pricePer, product));
+                            else
+                              widget.cartController.remove(product: product);
+                          });
+                        },
+                        wishlistOnPress: (product) {
+                          setState(() {
+                            product.isWishlisted = !product.isWishlisted;
+                          });
+                        },
+                      );
+                    },
+                  )
+                : Center(child: CircularProgressIndicator.adaptive())))
+      ]),
+      PopupMenu(
+        showMenu: this.showMenu,
+        imageOnPressed: () => setState(() {
+          this.showMenu = !this.showMenu;
+        }),
+        circleImage: NetworkImage(widget.currentCustomer.user.photoUrl),
+        title: widget.currentCustomer.user.displayName.split(' ')[0].trim(),
+        menuItems: [
+          CustomPopupMenuItem(
+              onPressed: () {},
+              icon: Icons.favorite_border,
+              label: "Wishlisted"),
+          CustomPopupMenuItem(
+              onPressed: () {},
+              icon: FlutterIcons.package_fea,
+              label: "Orders"),
+          CustomPopupMenuItem(
+              onPressed: () {},
+              icon: Icons.settings_outlined,
+              label: "Settings"),
+          CustomPopupMenuItem(
+              onPressed: () {},
+              icon: FlutterIcons.logout_ant,
+              label: "Logout",
+              color: Color.fromRGBO(255, 102, 102, 1.0))
+        ],
+      )
     ]);
   }
 }
